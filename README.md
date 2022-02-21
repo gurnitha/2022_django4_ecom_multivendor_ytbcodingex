401c9d4a546b83e0fb953aad6b6a6b10ba6546dc
ing <ingafter60@outlook.com>
2022-02-21 22:19:55 +0700

    3. Creating paths for templates, static and media files

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ BUILDING MULTIVENDOR ECOMMERCE USING DJANGO 4
 
 #### 2. Create a new app 'app/main'
 
+#### 3. Creating paths for templates, static and media files
+
+        modified:   config/settings.py
+        modified:   config/urls.py
--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -34,7 +36,7 @@ INSTALLED_APPS = [
 
     # Locals/new
     'app.main.apps.MainConfig',
-    
+
     # Default apps
     'django.contrib.admin',
     'django.contrib.auth',
@@ -59,7 +61,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,9 +122,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# http://127.0.0.1:8000/static/css/style.css
+STATIC_URL  = '/static/' 
+STATIC_ROOT = '/static/' 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+
+# Media url and root
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
